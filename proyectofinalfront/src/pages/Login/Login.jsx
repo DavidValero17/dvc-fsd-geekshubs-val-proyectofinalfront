d97d3459b564b60b0071bfad216240ca81a5ad85
David Valero <davidvalero17@hotmail.com>
2023-05-13 23:04:20 +0200

    login solucionado, ahora saluda correctamente y redirige a home

--- proyectofinalfront/src/pages/Login/Login.jsx
+++ proyectofinalfront/src/pages/Login/Login.jsx
@@ -1,61 +1,148 @@
-import { useState } from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { logMe } from '../../services/apiCalls';
-import { login } from '../userSlice';
+import React from "react";
+
+import { useState, useEffect } from "react";
+import { logMe } from "../../services/apiCalls";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { login, userData } from "../userSlice";
+
+import { useNavigate } from "react-router-dom";
+import { InputLogin } from "../../common/LoginForm/LoginForm";
+
+import { decodeToken } from "react-jwt";
+
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 export const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await logMe({ email, password });
-      dispatch(login({ credentials: response.data }));
-      navigate('/');
-    } catch (error) {
-      setError('Wrong Credentials');
+  //Instancio Redux en modo escritura y lectura
+
+  const dispatch = useDispatch();
+  const credentialsRdx = useSelector(userData);
+  
+
+  const [credenciales, setCredenciales] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [welcome, setWelcome] = useState("");
+
+  useEffect(() => {
+    if (credentialsRdx.credentials?.token) {
+      //Si No token...home redirect
+      navigate("/");
     }
+  }, []);
+
+  const inputHandler = (e) => {
+    setCredenciales((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const checkError = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (credentialsRdx.credentials?.token) {
+      //Si No token...home redirect
+      navigate("/");
+    }
+  }, []);
+
+  const logeame = () => {
+    logMe(credenciales)
+      .then((respuesta) => {
+        let decoded = decodeToken(respuesta.data.data);
+
+        let datosBackend = {
+          token: respuesta.data.data,
+          usuario: decoded,
+        };
+
+        //Este es el momento en el que guardo en REDUX
+        dispatch(login({ credentials: datosBackend }));
+
+        //Una vez nos hemos logeado...mostramos mensaje de bienvenida...
+        setWelcome(`Bienvenid@ ${datosBackend.usuario.username}`);
+
+        //Redirección a Home
+
+        setTimeout(() => {
+          navigate("/");
+        }, 3000);
+      })
+      .catch((error) => alert("Se produjo un error al realizar el login"));
   };
 
   return (
-    <Container>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </Form.Group>
+    <div className="loginDesign">
+      {welcome !== "" ? (
+        <div>{welcome}</div>
+      ) : (
+        <Container fluid className="CenteredForm">
+          <Form>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formHorizontalEmail"
+            >
+              <Form.Label column sm={2}>
+                Email
+              </Form.Label>
+              <Col sm={10}>
+                <InputLogin
+                  type="text"
+                  name="email"
+                  placeholder="email@email.com"
+                  changeFunction={(e) => inputHandler(e)}
+                  blurFunction={(e) => checkError(e)}
+                />
+              </Col>
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </Form.Group>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formHorizontalPassword"
+            >
+              <Form.Label column sm={2}>
+                Pass
+              </Form.Label>
+              <Col sm={10}>
+                <InputLogin
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  changeFunction={(e) => inputHandler(e)}
+                  blurFunction={(e) => checkError(e)}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formHorizontalCheck"
+            >
+            </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-
-      {error && <p className="text-danger">{error}</p>}
-    </Container>
+            <Form.Group as={Row} className="mb-3">
+              <Col sm={{ span: 10, offset: 2 }}>
+                <Button onClick={() => logeame()}>Log Me</Button>
+              </Col>
+            </Form.Group>
+          </Form>
+        </Container>
+      )}
+    </div>
   );
 };
-
-export default Login;
