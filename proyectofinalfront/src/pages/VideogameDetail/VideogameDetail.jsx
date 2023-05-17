76564279e54afd60db531feb86e6273489b4bbd8
David Valero <davidvalero17@hotmail.com>
2023-05-17 23:00:42 +0200

    añadida la opcion de añadir a favoritos desde la vista detalle del videojuego. Una vez añadido el videojuego desaparecera la opcion de añadir a favoritos

--- proyectofinalfront/src/pages/VideogameDetail/VideogameDetail.jsx
+++ proyectofinalfront/src/pages/VideogameDetail/VideogameDetail.jsx
@@ -1,50 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import { useSelector } from "react-redux";
 import { userData } from "../userSlice";
-import { getVideogameById } from "../../services/apiCalls";
+import { getVideogameById, addToFavorite } from "../../services/apiCalls";
 import { useParams} from 'react-router-dom';
 
 import "./VideogameDetail.css"
 
 export const VideogameDetail = () => {
-    const { id } = useParams();
-    const userState = useSelector(userData);
-    const [videogameInfo, setVideogameInfo] = useState([]);
-  
-    const detailedVideogame = () => {
-      getVideogameById(userState.credentials.token, id)
-        .then((respuesta) => {
-          setVideogameInfo(respuesta.data.data);
-        })
-        .catch((error) => {
-          alert("Se produjo un error al cargar los videojuegos");
-        });
-    };
-    useEffect(() => {
-      detailedVideogame(userState.credentials.token);
-    }, [userState.credentials.token, id]);
-  
-    
-    if (!videogameInfo) {
-      return <div>Cargando...</div>;
-    }
+  const { id } = useParams();
+  const userState = useSelector(userData);
+  const [videogameInfo, setVideogameInfo] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const detailedVideogame = () => {
+    getVideogameById(userState.credentials.token, id)
+      .then((respuesta) => {
+        setVideogameInfo(respuesta.data.data);
+        setIsFavorite(respuesta.data.data.isFavorite); // Agregamos esta línea para verificar si el videojuego ya está en favoritos
+      })
+      .catch((error) => {
+        alert("Se produjo un error al cargar los videojuegos");
+      });
+  };
+
+  const handleAddToFavorites = () => {
+    addToFavorite({ videogame_id: id }, userState.credentials.token)
+      .then((response) => {
+        setIsFavorite(true);
+        alert(response.data.message);
+      })
+      .catch((error) => {
+        alert("Se produjo un error al añadir el videojuego a favoritos");
+      });
+  };
+
+  useEffect(() => {
+    detailedVideogame(userState.credentials.token);
+  }, [userState.credentials.token, id]);
+
+  if (!videogameInfo) {
+    return <div>Cargando...</div>;
+  }
 
   return (
     <div className="videogame-detail">
-    <Card>
-      <Card.Img variant="top" src={videogameInfo.image} alt={videogameInfo.description} />
-      <hr />
-      <Card.Body>
-        <Card.Title>{videogameInfo.title}</Card.Title>
+      <Card>
+        <Card.Img variant="top" src={videogameInfo.image} alt={videogameInfo.description} />
         <hr />
-        <Card.Text>{videogameInfo.description}</Card.Text>
-        <hr />
-        <Card.Text>Genre: {videogameInfo.genre}</Card.Text>
-        <hr />
-        <Card.Text>Year: {videogameInfo.year}</Card.Text>
-      </Card.Body>
-    </Card>
-  </div>
+        <Card.Body>
+          <Card.Title>{videogameInfo.title}</Card.Title>
+          <hr />
+          <Card.Text>{videogameInfo.description}</Card.Text>
+          <hr />
+          <Card.Text>Genre: {videogameInfo.genre}</Card.Text>
+          <Card.Text>Year: {videogameInfo.year}</Card.Text>
+          {!isFavorite && (
+            <Button variant="primary" onClick={handleAddToFavorites}>
+              Add to Favorites
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
--- proyectofinalfront/src/services/apiCalls.js
+++ proyectofinalfront/src/services/apiCalls.js
@@ -11,85 +11,85 @@ export const registerMe = async (body) => {
 };
 
 export const getProfile = async (token) => {
-    let config = {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    };
-    return await axios.get(`${root}/user/getprofile`, config);
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
   };
+  return await axios.get(`${root}/user/getprofile`, config);
+};
 
-  export const updateProfile = async (body, token) => {
-    let config = {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    };
-    return await axios.put(`${root}/user/getprofile/`, body, config);
+export const updateProfile = async (body, token) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
   };
+  return await axios.put(`${root}/user/getprofile/`, body, config);
+};
 
 export const getAllUsers = async (token) => {
-    let config = {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    };
-    return await axios.get(`${root}/user/profile/getallusers`, config);
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
+  return await axios.get(`${root}/user/profile/getallusers`, config);
+};
+
+export const getAllVideogames = async (token, filters = {}) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
   };
 
-  export const getAllVideogames = async (token, filters={}) => {
-    let config = {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    };
+  const { genre, title, year, multiplayer, online, developer } = filters;
+  let url = `${root}/user/getallvideogames?`;
 
-    const { genre, title, year, multiplayer, online, developer } = filters
-    let url = `${root}/user/getallvideogames?`
+  if (genre) {
+    url += `&genre=${genre}`;
+  }
 
-    if(genre){
-      url += `&genre=${genre}`
-    }
+  if (title) {
+    url += `&title=${title}`;
+  }
 
-    if(title){
-      url += `&title=${title}`
-    }
+  if (year) {
+    url += `&year=${year}`;
+  }
 
-    if(year){
-      url += `&year=${year}`
-    }
+  if (multiplayer) {
+    url += `&multiplayer=${multiplayer}`;
+  }
 
-    if(multiplayer){
-      url += `&multiplayer=${multiplayer}`
-    }
+  if (online) {
+    url += `&online=${online}`;
+  }
 
-    if(online){
-      url += `&online=${online}`
-    }
+  if (developer) {
+    url += `&developer=${developer}`;
+  }
 
-    if(developer){  
-      url += `&developer=${developer}`
-    }
+  return await axios.get(url, config);
+};
 
-    return await axios.get(url, config);
+export const getVideogameById = async (token, id) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
   };
+  return await axios.get(`${root}/user/getvideogamebyid/${id}`, config);
+};
 
-  export const getVideogameById = async (token, id) => {
-    let config = {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    };
-    return await axios.get(`${root}/user/getvideogamebyid/${id}`, config);
+export const addVideogame = async (body, token) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
   };
-
-  export const addVideogame = async (body, token) => {
-    let config = {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    };
-    return await axios.post(`${root}/user/addvideogame`, body, config);
+  return await axios.post(`${root}/user/addvideogame`, body, config);
 };
 
 export const updateVideogame = async (body, token) => {
@@ -99,4 +99,13 @@ export const updateVideogame = async (body, token) => {
     },
   };
   return await axios.put(`${root}/user/updatevideogame/`, body, config);
+};
+
+export const addToFavorite = async (body, token) => {
+  let config = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
+  return await axios.post(`${root}/user/addtofavorite`, body, config);
 };
